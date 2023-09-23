dadff804e39323111f0660dd287d43d50380e155
Yakub289 <Yakubsuleiman24@gmail.com>
2023-09-23 15:28:04 -0700

    Update

--- models/user.py
+++ models/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 """ holds class User """
+
 import hashlib
 import models
 from models.base_model import BaseModel, Base
@@ -16,8 +18,10 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user", cascade="all, delete-orphan")
-        reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
+        places = relationship("Place", backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete-orphan")
     else:
         email = ""
         password = ""
