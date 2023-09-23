29b45a8a3efb095704f59684b0bcae7002210024
Yakub289 <Yakubsuleiman24@gmail.com>
2023-09-23 16:28:14 -0700

    Update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,7 +4,7 @@
 connection."""
 
 import os
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
